d4a96ca24fa2b7070d762a19bf4bb5d26f38754b
Alexey Erzyamkin <alexey.erzyamkin@outlook.com>
2017-08-29 04:59:31 +0300

    Added send logic to client connection

---
+++ src/Server/ClientConnection.cs
@@ -0,0 +1,209 @@
+using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class ClientConnection
+    {
+        private const short Header = 0x0CAE;
+        private const int FullHeaderLength = 4;
+        private const int BufferSize = 1000;
+
+        private readonly Socket _socket;
+
+        //private readonly CancellationToken _cancellationToken;
+        private readonly byte[] _readBuffer = new byte[BufferSize];
+        private int _readOffset;
+
+        private readonly IProducerConsumerCollection<byte[]> _readChannel;
+        private readonly ConcurrentQueue<byte[]> _sendQueue = new ConcurrentQueue<byte[]>();
+
+        private Task _readTask;
+        // private Task _sendTask;
+        
+        private readonly object _sendTaskSync = new object();
+        private volatile bool _sendTaskWorking;
+
+        public ClientConnection(Socket socket, IProducerConsumerCollection<byte[]> readChannel)
+        {
+            _socket = socket;
+            _readChannel = readChannel;
+        }
+
+        public void Start()
+        {
+            _readTask = Task.Run(
+                async () =>
+                {
+                    try
+                    {
+                        while (true)
+                        {
+                            byte[] data = await ReadData();
+                            _readChannel.TryAdd(data);
+                        }
+                    }
+                    catch (InvalidOperationException)
+                    {
+                    }
+                }
+            );
+        }
+
+        public void Send(byte[] data)
+        {
+            _sendQueue.Enqueue(data);
+
+            if (!_sendTaskWorking)
+            {
+                lock (_sendTaskSync)
+                {
+                    if (!_sendTaskWorking)
+                    {
+                        Task
+                            .Run(
+                                async () =>
+                                {
+                                    while (_sendQueue.TryDequeue(out byte[] dataToSend))
+                                    {
+                                        await SendAsync(data);
+                                    }
+                                })
+                            .ContinueWith(
+                                state =>
+                                {
+                                    _sendTaskWorking = false;
+                                });
+                    }
+                }
+            }
+        }
+
+        private async Task<byte[]> ReadData()
+        {
+            byte[] data = null;
+            short length = 0;
+
+            int bytesRead;
+            while ((bytesRead = await ReceiveAsync()) > 0)
+            {
+                _readOffset += bytesRead;
+
+                if (length == 0)
+                {
+                    length = ParseHeader();
+                    if (length < 0)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+
+                if (length > 0 && length <= GetBytesRead())
+                {
+                    data = ParseData(length);
+
+                    break;
+                }
+            }
+
+            return data;
+        }
+
+        private Task<int> ReceiveAsync()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            _socket.BeginReceive(_readBuffer, _readOffset, _readBuffer.Length - _readOffset, SocketFlags.None, EndCallback, _socket);
+            
+            return tcs.Task;
+
+            void EndCallback(IAsyncResult asyncResult)
+            {
+                var socket = (Socket) asyncResult.AsyncState;
+                try
+                {
+                    int bytesRead = socket.EndReceive(asyncResult);
+                    tcs.TrySetResult(bytesRead);
+                }
+                catch (Exception ex)
+                {
+                    tcs.TrySetException(ex);
+                }
+            }
+        }
+
+        private int GetBytesRead()
+        {
+            return _readOffset - FullHeaderLength;
+        }
+
+        private short ParseHeader()
+        {
+            if (_readOffset < FullHeaderLength)
+            {
+                return 0;
+            }
+
+            short receivedHeader = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(_readBuffer, 0));
+            if (receivedHeader != Header)
+            {
+                return -1;
+            }
+
+            return IPAddress.NetworkToHostOrder(BitConverter.ToInt16(_readBuffer, 2));
+        }
+
+        private byte[] ParseData(short length)
+        {
+            if (GetBytesRead() < length)
+            {
+                return null;
+            }
+
+            // Optimize this
+            var result = new byte[length];
+            Array.Copy(_readBuffer, FullHeaderLength, result, 0, length);
+
+            int fullLength = FullHeaderLength + length;
+            int nextDataLength = _readOffset - fullLength;
+            Array.Copy(_readBuffer, fullLength, _readBuffer, 0, nextDataLength);
+
+            _readOffset = nextDataLength;
+
+            return result;
+        }
+
+        private Task<int> SendAsync(byte[] data)
+        {
+            var tcs = new TaskCompletionSource<int>();            
+            _socket.BeginSend(data, 0, data.Length, SocketFlags.None, EndCallback, _socket);
+
+            return tcs.Task;
+
+            void EndCallback(IAsyncResult asyncResult)
+            {
+                Socket socket = (Socket)asyncResult.AsyncState;
+                try
+                {
+                    int bytesRead = socket.EndSend(asyncResult);
+                    tcs.TrySetResult(bytesRead);
+                }
+                catch (Exception ex)
+                {
+                    tcs.TrySetException(ex);
+                }
+            }
+        }
+
+        private async Task DisconnectAsync()
+        {
+            await Task.Factory.FromAsync(
+                _socket.BeginDisconnect(false, null, null),
+                _socket.EndDisconnect
+            );
+        }
+    }
+}
--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -10,51 +10,27 @@ namespace Server
     class Program
     {
         static void Main(string[] args)
+            => MainAsync().GetAwaiter().GetResult();
+        
+        private static async Task MainAsync()
         {
             var server = new Server(10891, 1000);
 
-            server.Start2();
-
-            Console.WriteLine("Press ENTER to exit...");
-            Console.ReadLine();
-
-            server.Stop();
+            await server.Start();
         }
     }
 
     class Server
     {
-        private readonly ConcurrentBag<Connection> _connections = new ConcurrentBag<Connection>();
+        private readonly ConcurrentBag<ClientConnection> _connections = new ConcurrentBag<ClientConnection>();
+        private readonly ConcurrentQueue<byte[]> _readChannel = new ConcurrentQueue<byte[]>();
         private readonly int _port;
         private readonly int _backlog;
-
-        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
-        private readonly ManualResetEvent _stopEvent = new ManualResetEvent(false);
 
         public Server(int port, int backlog)
         {
             _port = port;
             _backlog = backlog;
-        }
-
-        public void Start2()
-        {
-            _stopEvent.Reset();
-
-            Task.Run(
-                async () =>
-                {
-                    try
-                    {
-                        await Start();
-                    }
-                    finally
-                    {
-                        _stopEvent.Set();
-                    }
-                },
-                _cancellationTokenSource.Token
-            );
         }
 
         public async Task Start()
@@ -66,23 +42,16 @@ namespace Server
 
                 Console.WriteLine("Server started on: " + listener.LocalEndPoint);
 
-                while (!_cancellationTokenSource.Token.IsCancellationRequested)
+                while (true)
                 {
                     Socket clientSocket = await AcceptAsync(listener);
 
-                    var connection = new Connection(clientSocket, _cancellationTokenSource.Token);
+                    var connection = new ClientConnection(clientSocket, _readChannel);
                     connection.Start();
 
                     _connections.Add(connection);
                 }
             }
-        }
-
-        public void Stop()
-        {
-            _cancellationTokenSource.Cancel();
-
-            _stopEvent.WaitOne();
         }
 
         private Task<Socket> AcceptAsync(Socket listener)
@@ -110,168 +79,5 @@ namespace Server
         }
     }
 
-    class Connection
-    {
-        private const short Header = 0x0CAE;
-        private const int FullHeaderLength = 4;
-        private const int BufferSize = 1000;
-
-        private readonly Socket _socket;
-        private readonly CancellationToken _cancellationToken;
-        private readonly byte[] _readBuffer = new byte[BufferSize];
-        
-        private int _readOffset;
-
-        public Connection(Socket socket, CancellationToken cancellationToken)
-        {
-            _socket = socket;
-            _cancellationToken = cancellationToken;
-        }
-
-        public void Start()
-        {
-            Task.Run(
-                async () =>
-                {
-                    try
-                    {
-                        while (!_cancellationToken.IsCancellationRequested)
-                        {
-                            byte[] data = await ReadData();
-                            await SendAsync(data);
-                        }
-                    }
-                    catch (InvalidOperationException)
-                    {
-                    }
-
-                    await DisconnectAsync();
-                },
-                _cancellationToken
-            );
-        }
-
-        private async Task<byte[]> ReadData()
-        {
-            byte[] data = null;
-            short length = 0;
-
-            int bytesRead;
-            while ((bytesRead = await ReceiveAsync()) > 0)
-            {
-                _readOffset += bytesRead;
-
-                if (length == 0)
-                {
-                    length = ParseHeader();
-                    if (length < 0)
-                    {
-                        throw new InvalidOperationException();
-                    }
-                }
-
-                if (length > 0 && length <= GetBytesRead())
-                {
-                    data = ParseData(length);
-
-                    break;
-                }
-            }
-
-            return data;
-        }
-
-        private Task<int> ReceiveAsync()
-        {
-            var tcs = new TaskCompletionSource<int>();
-            _socket.BeginReceive(_readBuffer, _readOffset, _readBuffer.Length - _readOffset, SocketFlags.None, EndCallback, _socket);
-            
-            return tcs.Task;
-
-            void EndCallback(IAsyncResult asyncResult)
-            {
-                var socket = (Socket) asyncResult.AsyncState;
-                try
-                {
-                    int bytesRead = socket.EndReceive(asyncResult);
-                    tcs.TrySetResult(bytesRead);
-                }
-                catch (Exception ex)
-                {
-                    tcs.TrySetException(ex);
-                }
-            }
-        }
-
-        private int GetBytesRead()
-        {
-            return _readOffset - FullHeaderLength;
-        }
-
-        private short ParseHeader()
-        {
-            if (_readOffset < FullHeaderLength)
-            {
-                return 0;
-            }
-
-            short receivedHeader = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(_readBuffer, 0));
-            if (receivedHeader != Header)
-            {
-                return -1;
-            }
-
-            return IPAddress.NetworkToHostOrder(BitConverter.ToInt16(_readBuffer, 2));
-        }
-
-        private byte[] ParseData(short length)
-        {
-            if (GetBytesRead() < length)
-            {
-                return null;
-            }
-
-            // Optimize this
-            var result = new byte[length];
-            Array.Copy(_readBuffer, FullHeaderLength, result, 0, length);
-
-            int fullLength = FullHeaderLength + length;
-            int nextDataLength = _readOffset - fullLength;
-            Array.Copy(_readBuffer, fullLength, _readBuffer, 0, nextDataLength);
-
-            _readOffset = nextDataLength;
-
-            return result;
-        }
-
-        private Task<int> SendAsync(byte[] data)
-        {
-            var tcs = new TaskCompletionSource<int>();            
-            _socket.BeginSend(data, 0, data.Length, SocketFlags.None, EndCallback, _socket);
-
-            return tcs.Task;
-
-            void EndCallback(IAsyncResult asyncResult)
-            {
-                Socket socket = (Socket)asyncResult.AsyncState;
-                try
-                {
-                    int bytesRead = socket.EndSend(asyncResult);
-                    tcs.TrySetResult(bytesRead);
-                }
-                catch (Exception ex)
-                {
-                    tcs.TrySetException(ex);
-                }
-            }
-        }
-
-        private async Task DisconnectAsync()
-        {
-            await Task.Factory.FromAsync(
-                _socket.BeginDisconnect(false, null, null),
-                _socket.EndDisconnect
-            );
-        }
-    }
+    
 }
